04fa582ac01ce9788edd2b19fa9206b363edb9ef
Michael Aumock <maumock@origami3.com>
2017-06-12 21:42:49 -0600

    clearing deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 npm run dist
-rsync -avr --exclude dist/node_modules --exclude node_modules ./dist/ app1:/opt/panther/dist
-scp package-prod.json app1:/opt/panther/dist/package.json
-ssh app1 'cd /opt/panther/dist; npm install'
 ssh app1 'pm2 restart index'
