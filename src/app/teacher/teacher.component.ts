76405478b0b284c60550b0f0120c2a1677f79ca6
Saša Prtvar <prtvar11@hotmail.com>
2017-06-12 21:05:31 -0600

    get request URL

--- src/app/teacher/teacher.component.ts
+++ src/app/teacher/teacher.component.ts
@@ -22,7 +22,7 @@ import { ActivatedRoute } from '@angular/router';
      
         <div class= "students" template="ngFor let user of users">
         <a href="#/student">
-        <img src="{{ user.img }}"/>
+        <img src="{{user.img}}"/>
         </a>
         {{ user.name }}
         
@@ -33,8 +33,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class TeacherComponent implements OnInit {
-  public users = [{ name: 'John Cena', img: 'a' },
-    { name: 'Oliver McClendon', img: 'b' }, { name: 'Billy Maddison', img: 'c' }];
+  public users = [{ name: 'John Cena', img: 'https://s-media-cache-ak0.pinimg.com/originals/2f/41/d5/2f41d5e49987c9fa9a4df9046c1ef8e9.jpg' },
+    { name: 'Oliver McClendon', img: 'https://a.dilcdn.com/bl/wp-content/uploads/sites/8/2011/10/the-tough-guy.jpg' }, 
+    { name: 'Billy Maddison', img: 'https://thumb1.shutterstock.com/display_pic_with_logo/531898/177207620/stock-photo-handsome-little-boy-demonstrating-his-muscles-177207620.jpg'}, 
+    { name: 'Pop Eye', img: 'https://thumbs.dreamstime.com/x/peuter-jong-geitje-18227113.jpg'},
+    { name: 'Bill Nye', img: 'https://thumbs.dreamstime.com/x/preschool-boy-holding-globe-16273023.jpg'},
+    { name: 'Suzy Smiles', img: 'http://www.parentingnation.in/Resources/articleimages/actual/668.jpg' }];
   constructor(
     public route: ActivatedRoute
   ) { }
