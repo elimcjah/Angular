02a989c21759c987e10f26ec5f9e48ed4a6add21
Davis Foderberg <dfoderberg@gmail.com>
2017-06-16 20:32:36 -0600

    added www folder and landing page

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 npm run build
 rsync -avr ./dist/ aws:/home/ubuntu/angular_project/dist/
 rsync -avr ./server-src/ aws:/home/ubuntu/angular_project/server-src/
+rsync -arv ./www/ aws:/home/ubuntu/angular_project/www/
 scp package.json aws:/home/ubuntu/angular_project/package.json
 ssh aws 'cd /home/ubuntu/angular_project/; npm install --prod'
 ssh aws 'pm2 restart index; pm2 show index'
