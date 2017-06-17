e102d836f53cba50eb8633ed6f645b8f26140da8
Saša Prtvar <prtvar11@hotmail.com>
2017-06-16 18:12:23 -0600

    merged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,9 @@ import { Component } from '@angular/core';
     <main style="display: flex-direction:column; height:100%;">
     <!-- core/navbar.component.html -->
     <div class="filler"></div>
-<nav class="navbar">
-    <div class="container-fluid">
-        <div class="navbar-header">
+<div class="navbar">
+     <div class="navbar-header">
+        
             <div><img id= "logo" src = "./assets/images/logo.png"></div>
             <a class="navbar-brand" href = "#/welcome"><button id="welcome">Welcome</button></a>
             <a class="navbar-brand" href = "#/home"><button id="student">Students</button></a>
@@ -20,9 +20,11 @@ import { Component } from '@angular/core';
             <a class="navbar-brand" href = "#/add"><button id="add">+ Student</button></a>
             <a class="navbar-brand"><button id="login">Login</button></a>
         </div>
-        
-    </div>
-</nav>
+        <div class="brnav">
+          <h2 id = "stunav">Student</h2>
+        </div>
+    
+</div>
       <router-outlet></router-outlet>
     </main>
   `
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,19 +12,19 @@ import { ActivatedRoute } from '@angular/router';
   <main style="display: flex-direction:column; height:100%;">
     <!-- core/navbar.component.html -->
     <div class="filler"></div>
-<nav class="navbar">
-    <div class="container-fluid">
+<div class="navbar">
+    <div class="brnav"></div>
         <div class="navbar-header">
-            <img src = "./images/logo.png">
-            <a class="navbar-brand"><button id="home">Welcome</button></a>
+            <div><img id= "logo" src = "./assets/images/logo.png"></div>
+            <a class="navbar-brand" href = "#/welcome"><button id="welcome">Welcome</button></a>
             <a class="navbar-brand" href = "#/home"><button id="student">Students</button></a>
             <a class="navbar-brand"><button id="games">Games</button></a>
             <a class="navbar-brand" href = "#/add"><button id="add">+ Student</button></a>
             <a class="navbar-brand"><button id="login">Login</button></a>
         </div>
         
-    </div>
-</nav>
+    
+</div>
       <router-outlet></router-outlet>
     </main>
 `
