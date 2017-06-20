9f8ad3bf765e593a652ca7a4c2052c600606b43d
Michael Francis <michaelmac@Michaels-MacBook-Air.local>
2017-06-19 21:28:11 -0600

    user login pic/name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; @Component({ selector: 'app-root', templateUrl: './app.component.html', styleUrls:
+['./app.component.css'], template: `
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+<main style="display: flex-direction:column; height:100%;">
+  <!-- core/navbar.component.html -->
+  <div class="filler"></div>
+  <div class="navbar">
 
-  template: `
-    
-    <main style="display: flex-direction:column; height:100%;">
-    <!-- core/navbar.component.html -->
-    <div class="filler"></div>
-<div class="navbar">
-     <div class="navbar-header">
-        
-            <div><img id= "logo" src = "./assets/images/logo.png"></div>
-            <a class="navbar-brand" href = "#/login"><button id="welcome">Welcome</button></a>
-            <a class="navbar-brand" href = "#/home"><button id="student">Students</button></a>
-            <a class="navbar-brand"><button id="games">Games</button></a>
-            <a class="navbar-brand" href = "#/add"><button id="add">+ Student</button></a>
-            <a class="navbar-brand"><button id="login">Login</button></a>
-        </div>
-        <div class="brnav">
-          <h2 id = "stunav">Student</h2>
-        </div>
-    
-</div>
-      <router-outlet></router-outlet>
-    </main>
-  `
-})
-export class AppComponent {
-  fullImagePath: string;
+    <div class="navbar-header">
 
-  constructor() {
-    this.fullImagePath = 'assets/images/workday.jpg';
-  }
-}
+      <div><img id="logo" src="./assets/images/logo.png"></div>
+      <a class="navbar-brand" href="#/login"><button id="welcome">Welcome</button></a>
+      <a class="navbar-brand" href="#/home"><button id="student">Students</button></a>
+      <a class="navbar-brand"><button id="games">Games</button></a>
+      <a class="navbar-brand" href="#/add"><button id="add">+ Student</button></a>
+      <a class="navbar-brand"><button id="login">Login</button></a>
+
+      <div><img id="teach1" src="./assets/images/teacher1.jpeg"></div>
+      <h4 id="teach-name">Ms. Niki</h4>
+
+    </div>
+    <div class="brnav">
+      <h2 id="stunav">Student</h2>
+    </div>
+
+  </div>
+  <router-outlet></router-outlet>
+</main>
+` }) export class AppComponent { fullImagePath: string; constructor() { this.fullImagePath = 'assets/images/workday.jpg';
+} }
