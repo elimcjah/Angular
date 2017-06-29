2f6a6a48a124db0fc1d0fa0468b4d330c2d5dc94
Davis Foderberg <dfoderberg@gmail.com>
2017-06-28 19:46:15 -0600

    updating css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,53 +9,59 @@ import { ActivatedRoute } from '@angular/router';
     selector: 'login',
     // style: ,
     template: `
-    <h1 id = "logintitle">Welcome Teachers & Parents</h1>
-    <div style="width: 500px; margin: 200px auto 0 auto; border-radius: 25px; border: 2px solid #6d4c41;">
-    <h2> Please login below to access your class</h2>
     <body>
-    <div class = "login-forms">
-    <label><b>Username</b></label>
-    <br>
-        <input [(ngModel)]="userdata.usname" #ctrl="ngModel" placeholder="Enter Username"><br>
-    <br>
-    <label><b>Password</b></label>
-    <br>
-    <input type="password" [(ngModel)]="userdata.pass" #ctrl="ngModel" placeholder="Enter Password"> <br> 
-    <br>
-    <a href="#/signup">Signup</a>
-    <br>
-    <br>
-    <input type="checkbox" checked="checked"> Remember me
-    <br>
-    <br>
-    <button (click)="sub()">Submit</button>
-    
-    <style> 
-    @media screen and (max-width: 460px) {
-    * {
-        background-color: lightgreen;
-    }
-}
-
-</style>        
-            <h1 id="logintitle">Welcome Teachers and Parents</h1>
+    <h1 id="logintitle">Welcome Teachers & Parents</h1>
+    <div style=" display:flex; justify-content: center;">
+        <div style="width: 80%; border-radius: 25px; border: 2px solid #6d4c41;">
             <h2> Please login below to access your class</h2>
-        
 
-       
-        <div>
-            <div class="col-2"> </div>
-            <div class="login-forms col-4">
-                <input [(ngModel)]="userdata.usname" #ctrl="ngModel" placeholder="Your Login"><br>
-                <br>
-                <input password [(ngModel)]="userdata.pass" #ctrl="ngModel" placeholder="Your Password"> <br>
-                <br>
-                <br>
-                <button (click)="clicked()">Submit</button>
-            </div>
-            <div class="col-6"> </div>
+            
+                <div class="login-forms">
+                    <label><b>Username</b></label>
+                    <br>
+                    <input [(ngModel)]="userdata.usname" #ctrl="ngModel" placeholder="Enter Username"><br>
+                    <br>
+                    <label><b>Password</b></label>
+                    <br>
+                    <input type="password" [(ngModel)]="userdata.pass" #ctrl="ngModel" placeholder="Enter Password"> <br>
+                    <br>
+                    <a href="#/signup">Signup</a>
+                    <br>
+                    <br>
+                    <input type="checkbox" checked="checked"> Remember me
+                    <br>
+                    <br>
+                    <button (click)="sub()">Submit</button>
+
+                </div>
         </div>
-    
+    </div>
+
+                    <!--<style>
+                        @media screen and (max-width: 460px) {
+                            * {
+                                background-color: lightgreen;
+                            }
+                        }
+                    </style>-->
+                    <!--<h1 id="logintitle">Welcome Teachers and Parents</h1>
+                    <h2> Please login below to access your class</h2>
+
+
+
+                    <div>
+                        <div class="col-2"> </div>
+                        <div class="login-forms col-4">
+                            <input [(ngModel)]="userdata.usname" #ctrl="ngModel" placeholder="Your Login"><br>
+                            <br>
+                            <input password [(ngModel)]="userdata.pass" #ctrl="ngModel" placeholder="Your Password"> <br>
+                            <br>
+                            <br>
+                            <button (click)="clicked()">Submit</button>
+                        </div>
+                        <div class="col-6"> </div>
+                    </div>-->
+                </body>
     
     `
 })
